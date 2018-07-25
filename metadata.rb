53830022bafa8c63f03b0d3ab70f35a40af33d71
Vincent van de Vorst <vincentvandevorst@gamestop.com>
2018-07-25 11:57:02 +0100

    Added script

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures test_jenkins_cookbook'
 long_description 'Installs/Configures test_jenkins_cookbook'
-version '0.1.4'
+version '0.1.5'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
